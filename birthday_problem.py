704540e850f987640ae6c34c4471077e378a8f93
Francesco Ramoni <francesco.ramoni@finleap.com>
2022-05-29 17:06:36 +0200

    Generalize to 'at least any k'

--- birthday_problem.py
+++ birthday_problem.py
@@ -22,11 +22,32 @@ def at_least_2(N):
     return num / den
 
 
+def at_least_k(k, N):
+    """
+    Compute the probability that at least k of N people share their birthday.
+    1. Calculate all partitions of the set {1, ..., N}, excluding the ones that only have subsets with less thank k items
+    2. For each partition of length L, we have 365*364*...*(365-(L-1)) choices
+    3. Sum all the choices for each partition ---> num
+    4. Divide by all possible configurations  365^N ---> dem
+    """
+
+    num = 0
+    for i in multiset_partitions(N):
+        if all([len(s) < k for s in i]):
+            continue
+        else:
+            Pi = prod([365 - j for j in range(len(i))])
+            num += Pi
+    den = pow(365, N)
+    return num / den
+
+
 if __name__ == '__main__':
     max = 100
     P = []
-    for i in tqdm(range(2, max+1)):
-        P.append(at_least_2(i))
+    k = 2
+    for i in tqdm(range(k, max+1)):
+        P.append(at_least_k(k, i))
     plt.figure()
     plt.plot(list(range(2, max+1)), P)
-    plt.savefig('output/birthday_problem_2.png')
+    plt.savefig('output/birthday_problem_{}_{}.png'.format(k, max))
